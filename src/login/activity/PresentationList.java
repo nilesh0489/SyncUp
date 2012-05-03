c4aecdd6aa0a58f66e09d404c09ea484cd86a3da
truncs <sarawgi.aditya@gmail.com>
2012-05-02 23:47:20 -0400

    Presentation List with async download task

--- src/login/activity/PresentationList.java
+++ src/login/activity/PresentationList.java
@@ -6,6 +6,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+
+import android.os.AsyncTask;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -25,89 +27,88 @@ import android.view.View;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class PresentationList extends ListActivity {
-	private ArrayList<ParcelablePresentation> ar;
-	private String loginId;
-	private String sessionKey;
-	
+    private ArrayList<ParcelablePresentation> ar;
+    private String loginId;
+    private String sessionKey;
+
     /** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	  super.onCreate(savedInstanceState);
-	
-	  ar = getIntent().getParcelableArrayListExtra("List");
-	  loginId = getIntent().getStringExtra("loginId");
-	  sessionKey = getIntent().getStringExtra("sessionKey");
-	  	  
-	  setListAdapter(new ArrayAdapter<ParcelablePresentation>(this, R.layout.main, ar));
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	  ListView lv = getListView();
-	  
-	  
-	  lv.setTextFilterEnabled(true);
+        ar = getIntent().getParcelableArrayListExtra("List");
+        loginId = getIntent().getStringExtra("loginId");
+        sessionKey = getIntent().getStringExtra("sessionKey");
 
-	  lv.setOnItemClickListener(new OnItemClickListener() 
-	  {
-	    public void onItemClick(AdapterView<?> parent, View view,
-	        int position, long id) 
-	    {
-	    	Presentation presentation = ar.get(position).getPresentation();
-	    	Toast.makeText(getApplicationContext(), presentation.getName() ,
-	          Toast.LENGTH_SHORT).show(); 
-	    	String url = "http://10.0.2.2:8080/presentation/" + presentation.getId();
-	    	String fileName = presentation.getName() + ".zip";
-	    	File output = new File(Environment.getExternalStorageDirectory(),
-					fileName);
-			if (output.exists()) {
-				output.delete();
-			}
+        setListAdapter(new ArrayAdapter<ParcelablePresentation>(this, R.layout.main, ar));
 
-			InputStream stream = null;
-			FileOutputStream fos = null;
-			try {
+        ListView lv = getListView();
 
-				HttpGet httpGet = new HttpGet(url);
-				HttpClient httpclient = new DefaultHttpClient();
-				httpGet.setHeader("login-id", loginId);
-				httpGet.setHeader("session-key", sessionKey);
-				HttpResponse response = httpclient.execute(httpGet);
-				stream = response.getEntity().getContent();
-				BufferedInputStream buf = new BufferedInputStream(stream);
-				System.out.println(Environment.getExternalStorageState());
-				fos = new FileOutputStream(output);
-				int next = -1;
-				while ((next = buf.read()) != -1) {
-					fos.write(next);
-				}
-				
-				String zipFile = Environment.getExternalStorageDirectory() + "/" + presentation.getName() + ".zip"; 
-				String unzipLocation = Environment.getExternalStorageDirectory() + "/SyncUp/"; 
-				
-				Decompress d = new Decompress(zipFile, unzipLocation);
-				d.unzip();
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				if (stream != null) {
-					try {
-						stream.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-				if (fos != null) {
-					try {
-						fos.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-	    }
-	  });
-	}
-	
-	
-	
+        lv.setTextFilterEnabled(true);
+
+        lv.setOnItemClickListener(new OnItemClickListener()
+        {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id)
+            {
+                Presentation presentation = ar.get(position).getPresentation();
+
+                String url = "http://10.0.2.2:8080/presentation/" + presentation.getId() + '/' + '0';
+                DownloadImageTask task = new DownloadImageTask();
+                task.execute(new String[] {url});
+
+            }
+        });
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, String> {
+        // Set this to the image id from doInBackGround task
+        // and depending on the image set the boolean in an array to True
+        // if the download is complete
+        int image_id;
+        protected String doInBackground (String ... urls) {
+            for (String url : urls) {
+                String fileName = "temp" + ".jpg";
+                File output = new File(Environment.getExternalStorageDirectory(),
+                        fileName);
+                if (output.exists()) {
+                    output.delete();
+                }
+
+                InputStream stream = null;
+                FileOutputStream fos = null;
+                try {
+
+                    HttpGet httpGet = new HttpGet(url);
+                    HttpClient httpclient = new DefaultHttpClient();
+                    httpGet.setHeader("login-id", loginId);
+                    httpGet.setHeader("session-key", sessionKey);
+                    HttpResponse response = httpclient.execute(httpGet);
+                    stream = response.getEntity().getContent();
+                    BufferedInputStream buf = new BufferedInputStream(stream);
+                    System.out.println(Environment.getExternalStorageState());
+                    fos = new FileOutputStream(output);
+                    int next = -1;
+                    while ((next = buf.read()) != -1) {
+                        fos.write(next);
+                    }
+                    fos.close();
+                    buf.close();
+                }
+                catch (Exception e) {
+
+                }
+            }
+            return null;
+        }
+
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), "DONE" ,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 
 }
